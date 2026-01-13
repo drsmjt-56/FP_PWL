5848ed6ae0eda9cc05f3ea93dfbd4531df606ded
dorastismjtk <dorastisimanjuntak27@gmail.com>
2026-01-13 12:41:51 +0700

    fix layout frontend

--- routes/web.php
+++ routes/web.php
@@ -3,34 +3,29 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+// BACKEND CONTROLLER
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProdukController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\KontakController;
 use App\Http\Controllers\PembayaranController;
-use App\Http\Controllers\KategoriController;
 
-//FRONTEND
+// FRONTEND CONTROLLER
 use App\Http\Controllers\Frontend\FrontendController;
 use App\Http\Controllers\Frontend\FrontendProdukController;
-use App\Http\Controllers\Frontend\FrontendKontakController;
 
-
-/*
-|--------------------------------------------------------------------------
-| AUTH
-|--------------------------------------------------------------------------
-*/
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return redirect()->route('frontend.home');
 });
 
-Route::get('/login', [AuthController::class, 'showLogin'])
-    ->middleware('guest')
-    ->name('login');
-
-Route::post('/login', [AuthController::class, 'login']);
+/*
+ AUTH
+--------------------------------------------------------------------------
+*/
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login.process');
 
 Route::post('/logout', function () {
     Auth::logout();
@@ -40,125 +35,66 @@ Route::post('/logout', function () {
 })->name('logout');
 
 /*
-|--------------------------------------------------------------------------
-| ADMIN / BACKEND
-|--------------------------------------------------------------------------
+ BACKEND / ADMIN
+--------------------------------------------------------------------------
 */
+Route::middleware('auth')
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
 
-Route::middleware('auth')->group(function () {
-
-    /*
-    |-------------------------
-    | Dashboard
-    |-------------------------
-    */
+    // DASHBOARD
     Route::get('/dashboard', [DashboardController::class, 'index'])
         ->name('dashboard');
 
-    Route::get('/dashboard/chart-data', [DashboardController::class, 'chartData']);
+    Route::get('/dashboard/chart-data', [DashboardController::class, 'chartData'])
+        ->name('dashboard.chart');
 
+    // PRODUK 
+    Route::get('/produk', [ProdukController::class, 'index'])->name('produk.index');
+    Route::get('/produk/create', [ProdukController::class, 'create'])->name('produk.create');
+    Route::post('/produk', [ProdukController::class, 'store'])->name('produk.store');
+    Route::get('/produk/{id}/edit', [ProdukController::class, 'edit'])->name('produk.edit');
+    Route::put('/produk/{id}', [ProdukController::class, 'update'])->name('produk.update');
+    Route::delete('/produk/{id}', [ProdukController::class, 'destroy'])->name('produk.destroy');
 
-    /*
-    |-------------------------
-    | PRODUK
-    |-------------------------
-    */
-    Route::get('/produk', [ProdukController::class, 'index'])
-        ->name('produk.index');
-
-    Route::get('/produk/create', [ProdukController::class, 'create'])
-        ->name('produk.create');
-
-    Route::post('/produk', [ProdukController::class, 'store'])
-        ->name('produk.store');
-
-    Route::get('/produk/{id}/edit', [ProdukController::class, 'edit'])
-        ->name('produk.edit');
-
-    Route::put('/produk/{id}', [ProdukController::class, 'update'])
-        ->name('produk.update');
-
-    Route::delete('/produk/{id}', [ProdukController::class, 'destroy'])
-        ->name('produk.destroy');
-
-
-    /*
-    |-------------------------
-    | KONTAK
-    |-------------------------
-    */
-    Route::get('/kontak', [KontakController::class, 'index'])
-        ->name('kontak.index');
-
-    Route::post('/kontak', [KontakController::class, 'store'])
-        ->name('kontak.store');
-
-    Route::get('/kontak/{id}/edit', [KontakController::class, 'edit'])
-        ->name('kontak.edit');
-
-    Route::put('/kontak/{id}', [KontakController::class, 'update'])
-        ->name('kontak.update');
-
-    Route::delete('/kontak/{id}', [KontakController::class, 'destroy'])
-        ->name('kontak.destroy');
-
-
-    /*
-    |-------------------------
-    | PEMBAYARAN 
-    |-------------------------
-    */
-    Route::get('/pembayaran', [PembayaranController::class, 'index'])
-        ->name('pembayaran.index');
-
-    Route::get('/pembayaran/{id}/edit', [PembayaranController::class, 'edit'])
-        ->name('pembayaran.edit');
-
-    Route::delete('/pembayaran/{id}', [PembayaranController::class, 'destroy'])
-        ->name('pembayaran.destroy');
-
-
-    /*
-    |-------------------------
-    | KATEGORI
-    |-------------------------
-    */
+    // KATEGORI 
     Route::resource('kategori', KategoriController::class);
-
-    // PEMBAYARAN
-    Route::get('/pembayaran', [PembayaranController::class, 'index'])->name('pembayaran.index');
-    Route::get('/pembayaran/{id}/edit', [PembayaranController::class, 'edit'])->name('pembayaran.edit');
-    Route::delete('/pembayaran/{id}', [PembayaranController::class, 'destroy'])->name('pembayaran.destroy');
 
     // KONTAK
     Route::get('/kontak', [KontakController::class, 'index'])->name('kontak.index');
     Route::get('/kontak/{id}/edit', [KontakController::class, 'edit'])->name('kontak.edit');
     Route::put('/kontak/{id}', [KontakController::class, 'update'])->name('kontak.update');
     Route::delete('/kontak/{id}', [KontakController::class, 'destroy'])->name('kontak.destroy');
+
+    // PEMBAYARAN
+    Route::get('/pembayaran', [PembayaranController::class, 'index'])->name('pembayaran.index');
+    Route::get('/pembayaran/{id}/edit', [PembayaranController::class, 'edit'])->name('pembayaran.edit');
+    Route::delete('/pembayaran/{id}', [PembayaranController::class, 'destroy'])->name('pembayaran.destroy');
 });
 
+/*
+FRONTEND
+--------------------------------------------------------------------------
+*/
 
-//FRONTEND
-// HOME
-Route::get('/', [FrontendController::class, 'home'])
-    ->name('frontend.home');
+//HOME
+Route::get('/home', [FrontendController::class, 'home'])->name('frontend.home');
 
-// ABOUT
-Route::get('/about', [FrontendController::class, 'about'])
-    ->name('frontend.about');
+//ABOUT
+Route::get('/about', [FrontendController::class, 'about'])->name('frontend.about');
 
-// KONTAK
+//CARA PESAN
 Route::get('/cara_pesan', [FrontendController::class, 'caraPesan'])
     ->name('cara_pesan');
 
 Route::post('/kontak', [FrontendKontakController::class, 'store'])
 ->name('frontend.kontak.store');
 
-Route::get('/kontak', function () {
-    return view('frontend.kontak');})
-    ->name('frontend.kontak'); //selvie
 
-
-//PRODUK
+// PRODUK FRONTEND
 Route::get('/produk', [FrontendProdukController::class, 'index'])
     ->name('frontend.produk');
+
+//KONTAK
+Route::get('/kontak', [FrontendController::class, 'kontak'])->name('frontend.kontak');
